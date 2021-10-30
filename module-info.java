d0240f657ca2004844b0a84839187ed4a0fde8b2
Engo42 <73175817+Engo42@users.noreply.github.com>
2021-10-30 13:17:28 +1000

    Add files via upload

---
+++ module-info.java
@@ -0,0 +1,8 @@
+module com.example.demo {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires kotlin.stdlib;
+
+    requires org.kordamp.bootstrapfx.core;
+    requires java.desktop;
+}
